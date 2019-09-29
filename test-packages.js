b7e64b35ceb42f75be0057fee9efbde177698e78
Marco Montalbano <marcomontalbano.work@gmail.com>
2019-09-29 11:19:37 +0200

    Create group of tests based on packages

---
+++ test-packages.js
@@ -0,0 +1,46 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+
+const { readdirSync, statSync } = require('fs');
+const { resolve } = require('path');
+
+const isDirectory = (source) => statSync(source).isDirectory();
+
+const walkSync = (dir, startDir = dir) => {
+    let filelist = [];
+    const files = readdirSync(dir);
+
+    files.forEach((file) => {
+        if (isDirectory(dir + file)) {
+            filelist = [...filelist, ...walkSync(`${dir}${file}/`, startDir)];
+            return;
+        }
+
+        const isNodeModules = /node_modules/.test(dir);
+        const isTestFile = /\.test\.js$/.test(file);
+
+        if (!isNodeModules && isTestFile) {
+            filelist.push(`${dir}${file}`);
+        }
+    });
+
+    return filelist;
+};
+
+const getPackages = () => {
+    const packagesFolder = resolve('packages');
+    return readdirSync(packagesFolder).map((name) => resolve(packagesFolder, name)).filter(isDirectory);
+};
+
+const describePackage = (name, dirname) => {
+    describe(name, () => {
+        walkSync(`${dirname}/`).forEach(require);
+    });
+};
+
+getPackages().forEach((packagePath) => {
+    const packageJson = resolve(packagePath, 'package.json');
+    const { name } = require(packageJson);
+
+    describePackage(name, packagePath);
+});
