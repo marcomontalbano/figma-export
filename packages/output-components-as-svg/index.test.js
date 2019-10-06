a9a1c922001ca3c24734fbee36abff6d401fd4b3
Marco Montalbano <marcomontalbano.work@gmail.com>
2019-10-06 20:30:28 +0200

    Add unit test for output-as-svg

---
+++ packages/output-components-as-svg/index.test.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+
+const { expect } = chai;
+
+const fs = require('fs');
+
+const utils = require('../core/lib/utils');
+const figmaDocument = require('../core/lib/utils.test');
+
+const outputter = require('./index');
+
+describe('outputter as svg', () => {
+    let sandbox;
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('should export all components into svg files', async () => {
+        const writeFileSync = sandbox.stub(fs, 'writeFileSync');
+        const pages = utils.getPages({ children: [figmaDocument.page1] });
+
+        await outputter({
+            output: 'output',
+        })(pages);
+
+        expect(writeFileSync).to.be.calledTwice;
+        expect(writeFileSync.firstCall).to.be.calledWithMatch('output/Figma-Logo.svg');
+        expect(writeFileSync.secondCall).to.be.calledWithMatch('output/Search.svg');
+    });
+});
