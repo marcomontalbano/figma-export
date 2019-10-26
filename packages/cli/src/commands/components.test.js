3a69b31a7d83a49eedaf5c19d653817be2c218ff
Marco Montalbano <marcomontalbano.work@gmail.com>
2019-10-26 10:32:06 +0200

    Update 'cli' tests

---
+++ packages/cli/src/commands/components.test.js
@@ -0,0 +1,37 @@
+const { expect, test } = require('@oclif/test');
+
+const figma = require('@figma-export/core');
+
+const utils = require('../utils');
+
+describe('components', () => {
+    beforeEach(() => {
+        sinon.stub(figma, 'setToken');
+        sinon.stub(utils, 'mkdirRecursive');
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('should stdout a proper message with a fileId and an outputter', () => {
+        sinon.stub(figma, 'exportComponents').returns(Promise.resolve());
+
+        test
+            .stdout()
+            .command(['components', 'RSzpKJcnb6uBRQ3rOfLIyUs5', '-O', '@figma-export/output-components-as-svg'])
+            .it((ctx) => {
+                expect(ctx.stdout).to.contain('Exporting RSzpKJcnb6uBRQ3rOfLIyUs5 with [] as [@figma-export/output-components-as-svg]');
+            });
+    });
+
+    it('should throw an error with invalid fields (rejection)', () => {
+        sinon.stub(figma, 'exportComponents').returns(Promise.reject(new Error('Something went wrong')));
+
+        test
+            .stdout()
+            .command(['components', 'RSzpKJcnb6uBRQ3rOfLIyUs5', '-O', '@figma-export/output-components-as-svg'])
+            .exit(true)
+            .it();
+    });
+});
