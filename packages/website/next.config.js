691c8c1fa520699d4d15a4f1bafa12b5bfeb3227
Marco Montalbano <marcomontalbano.work@gmail.com>
2024-02-26 22:36:36 +0100

    fix: remove 'distDir' from next config file

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -7,8 +7,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
-  reactStrictMode: true,
-  distDir: 'dist'
+  reactStrictMode: true
 }
 
 module.exports = nextConfig;
