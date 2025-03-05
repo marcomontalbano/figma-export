334058a41fa142e2261ac279833a8fd55f0d6fa9
Marco Montalbano <marcomontalbano.work@gmail.com>
2025-03-05 01:14:49 +0100

    deps: update all dependencies to the latest major

--- .ncurc.js
+++ .ncurc.js
@@ -4,6 +4,7 @@ module.exports = {
     if (
       (name === '@types/node' &&
         Number.parseInt(upgradedVersionSemver?.major) >= 22) ||
+      // https://github.com/nock/nock/issues/2789
       (name === 'nock' && Number.parseInt(upgradedVersionSemver?.major) >= 14)
     ) {
       return false;
